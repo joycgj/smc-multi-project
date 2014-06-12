5ba2ef02ef6c69a27cbc8594dcfe2a802fdfbb4a
gaojiechen <gaojiechen@sohu-inc.com>
2014-06-12 14:25:10 +0800

    zookeeper

---
+++ joy-zookeeper/src/main/java/com/zk/ZKConstant.java
@@ -0,0 +1,13 @@
+package com.zk;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gaojiechen
+ * Date: 14-6-8
+ * Time: 下午4:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class ZKConstant {
+    public static final String ZK_SERVER = "10.1.156.26";
+
+}
