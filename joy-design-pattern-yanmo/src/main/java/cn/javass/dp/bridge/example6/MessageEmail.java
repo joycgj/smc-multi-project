664937ea980af8bece38e01a64b2458e89d86176
gaojiechen <gaojiechen@sohu-inc.com>
2014-12-17 10:03:11 +0800

    yanmo

---
+++ joy-design-pattern-yanmo/src/main/java/cn/javass/dp/bridge/example6/MessageEmail.java
@@ -0,0 +1,12 @@
+package cn.javass.dp.bridge.example6;
+
+/**
+ * 以Email的方式发送消息
+ * 24.2.4
+ */
+public class MessageEmail implements MessageImplementor {
+    @Override
+    public void send(String message, String toUser) {
+        System.out.println("使用Email的方式，发送消息'" + message + "'给" + toUser);
+    }
+}
