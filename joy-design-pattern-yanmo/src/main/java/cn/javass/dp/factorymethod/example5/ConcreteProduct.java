5ee30b729f1f765cc2b0e39059e6856c0f3b5c97
gaojiechen <gaojiechen@sohu-inc.com>
2014-12-18 10:07:10 +0800

    yanmo

---
+++ joy-design-pattern-yanmo/src/main/java/cn/javass/dp/factorymethod/example5/ConcreteProduct.java
@@ -0,0 +1,12 @@
+package cn.javass.dp.factorymethod.example5;
+
+public class ConcreteProduct implements Product {
+
+    @Override
+    public void setProduct1(Product1 p1) {
+    }
+
+    @Override
+    public void setProduct2(Product2 p2) {
+    }
+}
---
+++ joy-design-pattern-yanmo/src/main/java/cn/javass/dp/factorymethod/example6/C2.java
@@ -0,0 +1,8 @@
+package cn.javass.dp.factorymethod.example6;
+
+public class C2 implements C1 {
+
+    public void tc() {
+        // TODO Auto-generated method stub
+    }
+}
