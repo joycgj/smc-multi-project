b114221728ea9c41f2db32121feb5b8739844d78
gaojiechen <gaojiechen@sohu-inc.com>
2015-01-12 18:35:00 +0800

    thinking in java

---
+++ joy-thinking-in-java-4th/src/main/java/joy/MyThreadLocal2.java
@@ -0,0 +1,20 @@
+package joy;
+
+/**
+ * Created by gaojiechen on 2015/1/12.
+ */
+public class MyThreadLocal2 {
+    public static final ThreadLocal userThreadLocal = new ThreadLocal();
+
+    public static void set(Context user) {
+        userThreadLocal.set(user);
+    }
+
+    public static void unset() {
+        userThreadLocal.remove();
+    }
+
+    public static Context get() {
+        return (Context) userThreadLocal.get();
+    }
+}
