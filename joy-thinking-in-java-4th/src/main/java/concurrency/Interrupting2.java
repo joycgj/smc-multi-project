19ecc74cadf785f22dc59f39a54063043cf0fe35
chengaojie_home <chen_gao_jie@hotmail.com>
2015-01-13 23:16:02 +0800

    thinking

---
+++ joy-thinking-in-java-4th/src/main/java/concurrency/Interrupting2.java
@@ -0,0 +1,46 @@
+package concurrency;
+
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static net.mindview.util.Print.print;
+
+class BlockedMutex {
+    private Lock lock = new ReentrantLock();
+
+    public BlockedMutex() {
+        lock.lock();
+    }
+
+    public void f() {
+        try {
+            lock.lockInterruptibly();
+            print("lock acquired in f()");
+        } catch (InterruptedException e) {
+            print("Interrupted from lock acquisition in f()");
+        }
+    }
+}
+
+class Blocked2 implements Runnable {
+    BlockedMutex blocked = new BlockedMutex();
+
+    @Override
+    public void run() {
+        print("Waiting for f() in BlockedMutex");
+        blocked.f();
+        print("Broken out of blocked call");
+    }
+}
+
+public class Interrupting2 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new Blocked2());
+        t.start();
+        TimeUnit.SECONDS.sleep(8);
+        System.out.println("Issuing t.interrupt");
+        t.interrupt();
+    }
+}
